6cff8f784c44910caba4bd07c5575c43a78bc6fd
Sunn24 <114208094+Sunn24@users.noreply.github.com>
2023-12-30 16:41:44 +0700

    Update Integral Day.js

--- Integral Day.js
+++ Integral Day.js
@@ -1,31 +1,35 @@
 function onEdit(e) {
   var sheet = SpreadsheetApp.getActiveSheet();
-  var sheets = SpreadsheetApp.getActiveSpreadsheet().getSheets();
-  var cellValue = sheet.getRange('D3'); // Choose cell value
-  var email = Session.getActiveUser().getEmail();
-  var me = Session.getEffectiveUser();
-  var check = sheet.getRange('D1');
-  var st = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Timestamp");
-  st.appendRow([new Date(),email,e.range.getSheet().getName(),e.range.getA1Notation()]); // Timestamp
+  // Get email
+  var email = Session.getActiveUser().getEmail(); // users
+  var me = Session.getEffectiveUser(); // owner
+  // Recording timestamp with email
+  var timestampsheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Timestamp"); // Calling Timestamp sheet
+  timestampsheet.appendRow([new Date(),email,e.range.getSheet().getName(),e.range.getA1Notation()]); // Timestamp recording
+  // e.range.getRow(); if get only row of event
+  // e.range.getColumn(); if get only column of event
   ///// Protect Sheet by user email.
-  if (check.isChecked()&&sheet.getRange('E1').isBlank()){
+  var check = sheet.getRange('D1');
+  var cellValue = sheet.getRange('D3'); // Choose cell value
+  if (check.isChecked() && sheet.getRange('E1').isBlank()){
     sheet.getRange('E1').setValue(email);
     var protection = sheet.protect()
     protection.addEditor(me);
-    protection.removeEditors(protection.getEditors());
-    protection.setDomainEdit(false);
+    protection.removeEditors(protection.getEditors()); // remove another editors
+    protection.setDomainEdit(false); // Organization can't edit this sheet
     protection.addEditor(email) //.setDescription("Only " + email + " has edit rights");
   }
   // else if (check.isChecked()&&!sheet.getRange('E1').isBlank()){
   //   sheet.getRange('E1').setValue(email);
   // }
-  else if(!(check.isChecked()||sheet.getRange('E1').isBlank())){
-    sheet.getRange('E1').clearContent();
+  else if( !(check.isChecked() || sheet.getRange('E1').isBlank()) ){
+    sheet.getRange('E1').clearContent(); // clear cell
     var protection = sheet.protect()
     protection.addEditor(me);
-    protection.removeEditors(protection.getEditors());
-    protection.setDomainEdit(true);
+    protection.removeEditors(protection.getEditors()); // remove another editors
+    protection.setDomainEdit(true); // Organization can edit this sheet
   }
+
   ///// Choose MAJOR and set sheet name.
   if (check.isChecked()&&cellValue.isBlank()){
   sheet.showRows(2,2);
@@ -37,13 +41,7 @@ function onEdit(e) {
     sheet.setFrozenRows(6);
   }
   ///// Drop-down for choosing major (Data validation).
-  var out = new Array()
-  for (var i=0 ; i<sheets.length ; i++) out.push(sheets[i].getName());
-  var select = sheet.getRange('D3');
-  var allmajor = ["AS1", "AS2", "AS3", "AS4", "IMDS1", "IMDS2", "IMDS3", "IMDS4","MA2","MA3","MA4","GRAD"];
-  var major = allmajor.filter(x => !out.includes(x));//
-  var rule = SpreadsheetApp.newDataValidation().requireValueInList(major).build();
-  select.setDataValidation(rule);
+  ChooseMAJOR()
   ///// Sorting formula in the Conclusion sheet.
   var major2 = allmajor.filter(x => out.includes(x));//in
   var constantsSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Conclusion");
@@ -61,6 +59,19 @@ function onEdit(e) {
   if (check2.isChecked()){
     sheet.hideSheet();
   }
+
+function ChooseMAJOR(){
+  var sheets = SpreadsheetApp.getActiveSpreadsheet().getSheets(); // Get all Sheets in this Spreadsheet
+  var out = new Array()
+  for (var i=0 ; i<sheets.length ; i++) out.push(sheets[i].getName());
+  var select = sheet.getRange('D3');
+  var allmajor = ["AS1", "AS2", "AS3", "AS4", "IMDS1", "IMDS2", "IMDS3", "IMDS4","MA2","MA3","MA4","GRAD"];
+  var major = allmajor.filter(x => !out.includes(x)); // intersection of allmajor and out
+  var rule = SpreadsheetApp.newDataValidation().requireValueInList(major).build();
+  select.setDataValidation(rule); // dropdown 
+}
+
+  
 
 // function Hide() {
 //   var spreadsheet = SpreadsheetApp.getActive();
